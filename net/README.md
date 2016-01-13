8be45a5ab9c92b100c2a14da28a78d651b976f5b
Zhao.Kai <zeakey@126.com>
2016-01-13 19:05:27 +0800

    argparse

---
+++ net/README.md
@@ -0,0 +1,16 @@
+###Net Surgery
+
+=====
+####weight2mat.py
+
+transform model weights to `.mat`
+
+* usage
+
+```bash
+python weights2mat.py --caffe_root CAFFE_ROOT --model XX.caffemodel --deploy deploy.prototxt --mat xx.mat
+```
+
+* help
+
+use `python weights2mat.py --help` for help
--- net/weights2mat.py
+++ net/weights2mat.py
@@ -1,17 +1,26 @@
 import numpy as np
 import scipy.io as io
 import sys
+import argparse
+from os.path import normpath, join, split
 #options:
 #caffe root folder
-caffe_root = '/home/server002/zk/caffe/'
-sys.path.insert(0, caffe_root + 'python/')
+parser = argparse.ArgumentParser(description='transform model weights to mat')
+parser.add_argument('--caffe_root', type=str, help='caffe root folder', default='~/caffe/')
+parser.add_argument('--model', type=str, help='source model', default='model.caffemodel')
+parser.add_argument('--deploy', type=str, help='net deplot prototxt', default='deploy.prototxt')
+parser.add_argument('--mat', type=str, help='mat file to save weights', default='model.caffemodel')
+args = parser.parse_args()
+
+caffe_root = args.caffe_root
+sys.path.insert(0, join(caffe_root, 'python'))
 import caffe
 #trained model
-model_weights = '/home/server002/zk/github/dmil/examples/stage1/DS_iter_20000.caffemodel'
+model_weights = args.model
 #deplot prototxt
-net_deploy = '/home/server002/zk/github/dmil/examples/stage1/vgg_deploy.prototxt'
+net_deploy = args.deploy
 #saved .mat filename
-sav_mat_fn = 'weights'
+sav_mat_fn = args.mat
 
 caffe.set_mode_cpu()
 net = caffe.Net(net_deploy, model_weights, caffe.TEST)
